ae808a6f47d4ace543bede6f3f5d652347d7baaa
Roberto Hori Filho <robertohori@hotmail.com>
2021-10-29 14:02:23 -0400

    Updates

---
+++ precache-manifest.7fe06f6a1ca9feb7b369c937cc8007ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "700b991b4b6952b945718c34c9b7894b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b754e4c94193fe0683b",
+    "url": "/static/css/2.5aa997f2.chunk.css"
+  },
+  {
+    "revision": "ab28842ba0bd59863f4c",
+    "url": "/static/css/main.437d587a.chunk.css"
+  },
+  {
+    "revision": "7b754e4c94193fe0683b",
+    "url": "/static/js/2.ab0c8fd3.chunk.js"
+  },
+  {
+    "revision": "ab28842ba0bd59863f4c",
+    "url": "/static/js/main.875299f5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
