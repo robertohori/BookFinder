42a3ecd198204fc7a2777ffa7340e572d3c1bad1
Roberto Hori Filho <robertohori@hotmail.com>
2021-10-29 15:59:30 -0400

    changes

--- src/routes.js
+++ src/routes.js
@@ -1,19 +1,18 @@
 import React from "react";
-import { HashRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 // Configure routes
 import Home from "./screens/home";
 
 export default (
   <>
-    <HashRouter basename={process.env.PUBLIC_URL}>
-      <Route
-        render={({ location }) => (
-          <Switch location={location}>
-            <Route exact path="/" component={Home} />
-          </Switch>
-        )}
-      />
-    </HashRouter>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+    ,
   </>
 );
