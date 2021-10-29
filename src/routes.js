3ab8271f527abc0ea332eee91598345f7166c1ce
Roberto Hori Filho <robertohori@hotmail.com>
2021-10-29 11:09:36 -0400

    Revert "Revert "Project Book Finder""
    This reverts commit caeaeca3f2c64356888c47793f8e50c472bb0ff2.

---
+++ src/routes.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+// Configure routes
+import Home from "./screens/home";
+
+export default (
+  <BrowserRouter>
+    <Switch>
+      <Route path="/">
+        <Home />
+      </Route>
+    </Switch>
+  </BrowserRouter>
+);
