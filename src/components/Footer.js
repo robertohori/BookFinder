caeaeca3f2c64356888c47793f8e50c472bb0ff2
Roberto Hori Filho <robertohori@hotmail.com>
2021-10-29 11:01:00 -0400

    Revert "Project Book Finder"
    This reverts commit a72a5689aa146687606f3ef7c694519f1ea553c8.

--- src/components/Footer.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const Footer = () => (
-  <footer id="app-footer" aria-label="Footer">
-    &copy; {new Date().getFullYear()}
-  </footer>
-);
-export default Footer;
--- src/screens/home/actionTypes.js
+++
@@ -1,4 +0,0 @@
-// Action types
-
-export const REQUEST_BOOKS = 'REQUEST_BOOKS'
-export const RECEIVE_BOOKS = 'RECEIVE_BOOKS'
