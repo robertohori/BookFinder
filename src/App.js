2fd202c8bbec8fccea4896863436be712ea2bdca
Roberto Hori Filho <robertohori@hotmail.com>
2021-10-29 17:40:37 -0400

    Changes

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <a class="skip-main" href="#main">
+      <a className="skip-main" href="#main">
         Skip to main content
       </a>
       <Provider store={store}>
