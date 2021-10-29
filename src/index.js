93fd2633272899c1b61fd803c1ce7d924811c8f4
Roberto Hori Filho <robertohori@hotmail.com>
2021-10-29 16:26:09 -0400

    Changes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-//import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/css/bootstrap.css";
 // Put any other imports below so that CSS from your
 // components takes precedence over default styles.
-//import "./index.css";
+import "./index.css";
 
 import App from "./App";
 //import * as serviceWorker from './serviceWorker';
